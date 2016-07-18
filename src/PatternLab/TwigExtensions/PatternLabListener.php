c84ae2721f20bff956967e4ca78759a7209dc730
Salem Ghoweri <Salem.Ghoweri@pega.com>
2016-07-18 15:27:33 -0400

    Updating Patternlab Twig Extensions to also include markdown tag / filters by default.

---
+++ src/PatternLab/TwigExtensions/PatternLabListener.php
@@ -0,0 +1,53 @@
+<?php
+
+/*!
+ * Twig Extensions Listener Class
+ *
+ * Copyright (c) 2016 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ * Adds Twig Extensions support to the Twig Pattern Engine
+ *
+ */
+
+namespace PatternLab\TwigExtensions;
+
+use \PatternLab\PatternEngine\Twig\TwigUtil;
+use \Aptoma\Twig\Extension\MarkdownExtension;
+use \Aptoma\Twig\Extension\MarkdownEngine;
+
+$engine = new MarkdownEngine\MichelfMarkdownEngine();
+
+class PatternLabListener extends \PatternLab\Listener {
+  
+  /**
+  * Add the listeners for this plug-in
+  */
+  public function __construct() {
+    
+    $this->addListener("twigPatternLoader.customize","addExtensions");
+    
+  }
+  
+  /**
+  * Add the extensions to the appropriate instance
+  */
+  public function addExtensions() {
+    
+    $instance = TwigUtil::getInstance();
+    $instance->addExtension(new \Twig_Extensions_Extension_Text());
+    if (function_exists('gettext')) {
+      $instance->addExtension(new \Twig_Extensions_Extension_I18n());
+    }
+    if (class_exists("Collator")) {
+      $instance->addExtension(new \Twig_Extensions_Extension_Intl());
+    }
+    $instance->addExtension(new \Twig_Extensions_Extension_Array());
+    $instance->addExtension(new \Twig_Extensions_Extension_Date());
+    TwigUtil::setInstance($instance);
+    
+    $instance->addExtension(new MarkdownExtension($engine));
+    
+  }
+  
+}
